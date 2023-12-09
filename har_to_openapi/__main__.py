8a1a83258fc0f11c28843237c28089244aefe6c4
Nick Loadholtes <nick@ironboundsoftware.com>
2023-12-09 15:06:45 -0500

    working like before

---
+++ har_to_openapi/__main__.py
@@ -0,0 +1,17 @@
+import sys, json
+from converter import har_to_openapi, validate_openapi_spec
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python har_to_openapi <path-to-your-file>")
+        sys.exit(1)
+
+    har_file_path = sys.argv[1]  # 'path/to/your/file.har'
+
+    openapi_spec = har_to_openapi(har_file_path)
+    print(json.dumps(openapi_spec, indent=2))
+
+    # Uncomment the line below to validate the generated OpenAPI spec
+    validate_openapi_spec(openapi_spec)
+   # with open(sys.argv[1] + ".openapi_spec.json", "w") as f:
+   #     json.dump(f, openapi_spec)
--- har_to_openapi/converter.py
+++ har_to_openapi/converter.py
@@ -51,19 +51,3 @@ def validate_openapi_spec(openapi_spec):
 
     validate(instance=openapi_spec, schema=schema_data)
 
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python har_to_openapi <path-to-your-file>")
-        sys.exit(1)
-
-    har_file_path = sys.argv[1]  # 'path/to/your/file.har'
-
-    openapi_spec = har_to_openapi(har_file_path)
-    print(json.dumps(openapi_spec, indent=2))
-
-    # Uncomment the line below to validate the generated OpenAPI spec
-    validate_openapi_spec(openapi_spec)
-   # with open(sys.argv[1] + ".openapi_spec.json", "w") as f:
-   #     json.dump(f, openapi_spec)
-
