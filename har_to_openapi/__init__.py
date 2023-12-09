cbef32cdaf9eba5f2cd06882280977ec3d0ac65c
Nick Loadholtes <nick@ironboundsoftware.com>
2023-12-09 14:46:09 -0500

    blacken and sort, pyproject cleanup

--- har_to_openapi/__init__.py
+++ har_to_openapi/__init__.py
@@ -1,50 +1,56 @@
 import json
-import requests
 import sys
+
+import requests
 from haralyzer import HarParser
 from openapi_schema_validator import validate
 
+
 def har_to_openapi(har_file_path):
-    with open(har_file_path, 'r', encoding='utf-8') as har_file:
+    with open(har_file_path, "r", encoding="utf-8") as har_file:
         har_data = json.load(har_file)
 
     parser = HarParser(har_data)
-    entries = parser.har_data.get('log', {}).get('entries', [])
+    entries = parser.har_data.get("log", {}).get("entries", [])
 
     openapi_spec = {
-        'openapi': '3.0.0',
-        'info': {
-            'title': 'Generated API',
-            'version': '1.0.0',
+        "openapi": "3.0.0",
+        "info": {
+            "title": "Generated API",
+            "version": "1.0.0",
         },
-        'paths': {},
+        "paths": {},
     }
 
     for entry in entries:
-        request = entry['request']
-        method = request['method'].lower()
-        url = request['url']
+        request = entry["request"]
+        method = request["method"].lower()
+        url = request["url"]
 
-        path = url.replace("http://", "").replace("https://", "").split('/', 1)[-1]
+        path = url.replace("http://", "").replace("https://", "").split("/", 1)[-1]
 
-        if path not in openapi_spec['paths']:
-            openapi_spec['paths'][path] = {}
+        if path not in openapi_spec["paths"]:
+            openapi_spec["paths"][path] = {}
 
-        openapi_spec['paths'][path][method] = {
-            'responses': {
-                '200': {
-                    'description': 'Successful response',
+        openapi_spec["paths"][path][method] = {
+            "responses": {
+                "200": {
+                    "description": "Successful response",
                 },
             },
         }
 
     return openapi_spec
 
+
 def validate_openapi_spec(openapi_spec):
-    schema_url = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.json'
+    schema_url = (
+        "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.json"
+    )
     schema_data = requests.get(schema_url).json()
 
     validate(instance=openapi_spec, schema=schema_data)
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -52,10 +58,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     har_file_path = sys.argv[1]  # 'path/to/your/file.har'
-    
+
     openapi_spec = har_to_openapi(har_file_path)
     print(json.dumps(openapi_spec, indent=2))
 
     # Uncomment the line below to validate the generated OpenAPI spec
     # validate_openapi_spec(openapi_spec)
-
